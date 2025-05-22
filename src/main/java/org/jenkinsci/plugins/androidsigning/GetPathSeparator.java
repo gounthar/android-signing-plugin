623ef1031d3c9ece9b7efc247c5063913cf44ffb
Bruno Verachten <gounthar@gmail.com>
2025-05-22 08:48:50 +0200

    Refactor GetPathSeparator to implement Callable interface
    Replaced MasterToSlaveFileCallable with Callable to align with modern remoting API and improved serialization. Added RoleChecker method for security compliance, ensuring compatibility with updated Jenkins remoting practices.

--- src/main/java/org/jenkinsci/plugins/androidsigning/GetPathSeparator.java
+++ src/main/java/org/jenkinsci/plugins/androidsigning/GetPathSeparator.java
@@ -1,15 +1,22 @@
 package org.jenkinsci.plugins.androidsigning;
 
-import hudson.remoting.VirtualChannel;
+import hudson.remoting.Callable;
+import org.jenkinsci.remoting.RoleChecker;
+
 import java.io.File;
 import java.io.IOException;
-import jenkins.MasterToSlaveFileCallable;
+import java.io.Serializable;
 
-public class GetPathSeparator extends MasterToSlaveFileCallable<String> {
-    private static final long serialVersionUID = 1;
+public class GetPathSeparator implements Callable<String, IOException>, Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Override
-    public String invoke(File f, VirtualChannel channel) throws IOException, InterruptedException {
+    public String call() throws IOException {
         return File.pathSeparator;
+    }
+
+    @Override
+    public void checkRoles(RoleChecker roleChecker) throws SecurityException {
+        // This callable does not require any specific roles, so we can leave this method empty.
     }
 }
